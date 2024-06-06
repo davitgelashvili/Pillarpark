1876e9c63d16b9de51ee527a30080da8b3d618ea
Davit Gelashvili <datoogelashvili@gmail.com>
2024-06-06 22:08:50 +0400

    change

--- landing/function/include.js
+++ landing/function/include.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-    $('head').load('./components/head.html');
-    $('#header').load('./components/header/header.html');
-    $('#slider').load('./components/slider/slider.html');
-    $('#popup').load('./components/popup/popup.html');
-})
+// $(document).ready(function(){
+//     $('head').load('./components/head.html');
+//     $('#header').load('./components/header/header.html');
+//     $('#slider').load('./components/slider/slider.html');
+//     $('#popup').load('./components/popup/popup.html');
+// })
