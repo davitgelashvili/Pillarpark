207ae935e851a213ec9d4203b786a9477b8045a5
Davit Gelashvili <datoogelashvili@gmail.com>
2024-06-09 23:32:04 +0400

    upload client component

--- client/assets/javascript/slider.js
+++ client/assets/javascript/slider.js
@@ -1,29 +1,50 @@
-// $(document).ready(function(){
-//     $('.slider--content').owlCarousel({
-//         loop:false,
-//         nav:false,
-//         dots: true,
-//         dotsContainer: '.slider__dots',
-//         responsive:{
-//             0:{
-//                 items:1
-//             }
-//         }
-//     })
+
+$(window).on("load",function() {
+    // main slider 
+    (() => {
+        $('.slider__content').owlCarousel({
+            loop:false,
+            nav:false,
+            dots: true,
+            dotsContainer: '.slider__dots',
+            responsive:{
+                0:{
+                    items:1
+                }
+            }
+        })
+        
+        $('.slider__dots--item').on('click', function() {
+            $('.slider__content').trigger('to.owl.carousel', [$(this).index(), 300]);
+            $('.slider__dots--item').removeClass('active');
+            $(this).addClass( 'active' );
+        })
+    })()
+
+    // project slider
+    const Slider = $('.owl-carousel')
+    $('.projects__slider').owlCarousel({
+        loop:false,
+        nav:false,
+        dots: false,
+        margin:40,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    })
+
+    $('.owl--prev').click(function() {
+        Slider.trigger('prev.owl.carousel');
+    })
     
-//     $('.slider__dots--item').on('click', function() {
-//         $('.slider--content').trigger('to.owl.carousel', [$(this).index(), 300]);
-//         $('.slider__dots--item').removeClass('active');
-//         $(this).addClass( 'active' );
-//     })
-    
-//     const Slider = $('.owl-carousel')
-    
-//     $('.slider--prev').click(function() {
-//         Slider.trigger('prev.owl.carousel');
-//     })
-    
-//     $('.slider--next').click(function() {
-//         Slider.trigger('next.owl.carousel');
-//     })
-// })
+    $('.owl--next').click(function() {
+        Slider.trigger('next.owl.carousel');
+    })
+
+
+});
